a62d700849a0ba73d27f576c2f71e887269f6272
jobbie007 <jobtyko@gmail.com>
2025-04-24 21:55:27 +0200

    sfml works

--- VisualNovelQuizGame/VisualNovelQuizGame.cpp
+++ VisualNovelQuizGame/VisualNovelQuizGame.cpp
@@ -3,7 +3,23 @@
 
 #include <iostream>
 #include <SFML/Graphics.hpp>
+
 int main()
-{
+{   
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Visual Novel Quiz Game");
+	sf::CircleShape shape(50.f);
+	shape.setFillColor(sf::Color::Green);
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
     std::cout << "Hello World!\n";
 }
